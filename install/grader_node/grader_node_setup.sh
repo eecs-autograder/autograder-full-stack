93233021ebe92838060f4f3401ac936d54e388fb
James P <jameslp@umich.edu>
2016-09-15 18:42:48 -0400

    moved grader node scripts to ag-server repo

--- install/grader_node/grader_node_setup.sh
+++
@@ -1,6 +0,0 @@
-sudo cp systemd_scripts/ag_media_root_sshfs.service systemd_scripts/postgres_tunnel.service systemd_scripts/redis_tunnel.service systemd_scripts/rabbitmq_tunnel.service /etc/systemd/system/
-
-sudo systemctl daemon-reload
-sudo systemctl enable ag_media_root_sshfs.service postgres_tunnel.service redis_tunnel.service rabbitmq_tunnel.service
-
-sudo systemctl start ag_media_root_sshfs.service postgres_tunnel.service redis_tunnel.service rabbitmq_tunnel.service
